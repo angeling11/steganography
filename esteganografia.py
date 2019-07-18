f42270a7d9abb4f4bbb9aa5bf93616f49d0c0382
AngelinG <angel-na@hotmail.es>
2019-07-18 16:03:35 -0500

    decoded function implemented...

--- esteganografia.py
+++ esteganografia.py
@@ -18,32 +18,55 @@ def bin2dec(strg):
 		s += chr(int(strg[i : i + 8], 2))
 	return s
 
+def writeLSB(im, text, name):
+	px = im.load()
+	width, height = im.size
+	c = 0
+	fin = False
+	for y in range(height):
+		for x in range(width):
+			val = []
+			for i in range(3):
+				if c >= len(text):
+					val.append(int(px[x, y][i]))
+					fin = True
+				else:
+					val.append(int("{:08b}".format(px[x, y][i])[:7] + text[c], 2))
+				c += 1
+			px[x, y] = tuple(val)
+			if fin:
+				im.save(name)
+				return
+
 def readLSB(im):
 	s = ""
 	px = im.load()
 	width, height = im.size
-	#print(height, width)
 	for y in range(height):
 		for x in range(width):
 			for i in range(3):
-				#print("{:08b}".format(px[x, y][i]), end=" ")
 				s += bin(px[x, y][i])[-1]
-				if len(s) % 8 == 0 and s[-8 : -1] == SEP:
-					return s
-		#print()
+				if len(s) % 8 == 0 and s[-8:] == SEP:
+					print
+					return s[:-8]
 	return s
 
 def encode(img, file):
 	print("Encoding...\n")
-	print("Finished...\nSaved as:")
+	ext = img.split(".")
+	name = "encoded." + ext[-1]
+	im = Image.open(img)
+	text = file2str(file)
+	writeLSB(im, dec2bin(text) + SEP, name)
+	print("Finished...\nSaved as: ", name, sep="")
 
 def decode(img):
 	print("Decoding...\n")
 	im = Image.open(img)
 	print("Message:")
 	print(bin2dec(readLSB(im)))
+	print("\nFinished...")
 
 encode("fox.png", "file")
-decode("fox.png")
-#print(dec2bin(file2str("file")))
-#print(bin2dec(dec2bin(file2str("file"))))
+print("\n\n")
+decode("encoded.png")
